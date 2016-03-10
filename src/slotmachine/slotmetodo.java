eb2d629fea5921a6b2d3981e87768fac40280035
horace-g <horacio.galdamez@unitec.edu>
2016-03-09 22:29:47 -0600

    fixing libraries issues

--- src/slotmachine/slotmetodo.java
+++ src/slotmachine/slotmetodo.java
@@ -8,14 +8,19 @@ import java.util.Random;
 import java.util.Iterator;
 import java.util.Set;
 
-
+import com.google.common.collect.Multiset;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.ImmutableMultiset;
+import com.google.common.collect.Multisets;
 
 public class slotmetodo {
 
     static int paga(List<String> reels) {
         Multiset<String> reelcount = HashMultiset.create();
-        reelcount.addall(reels);
+        reelcount.addAll(reels);
 
-        ImmutableMultiset<String> HighestCountFirst = Multisets.copyHighestCountFirst(reelcount);
+       // ImmutableMultiset<String> HighestCountFirst = Multisets.copyHighestCountFirst(reelcount);
+        
+    return 0;    
     }
 }
